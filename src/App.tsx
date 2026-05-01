a441f9580657b365aadd7644308aee1be96e0dcf
hdhwang <hadong0624@naver.com>
2026-05-01 10:49:40 +0900

    fix: 전역 ConfigProvider 통합 및 하드코딩된 배경색 제거를 통한 다크모드 버그 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,27 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import AppRoutes from './components/router';
-import { Layout, App as AntdApp } from 'antd';
-import { SettingsProvider } from './components/settings_context';
+import { Layout, App as AntdApp, ConfigProvider, theme } from 'antd';
+import { SettingsProvider, useSettings } from './components/settings_context';
 import { NotificationProvider } from './components/notification_context';
 import AntdGlobal from './components/antd_global';
+import koKR from 'antd/es/locale/ko_KR';
 
-const App: React.FC = () => {
+const AppContent: React.FC = () => {
+    const { settings } = useSettings();
+    const isDarkMode = settings.themeMode === 'dark';
+
     return (
-        <SettingsProvider>
+        <ConfigProvider
+            locale={koKR}
+            theme={{
+                algorithm: isDarkMode ? theme.darkAlgorithm : theme.defaultAlgorithm,
+                token: {
+                    motionDurationSlow: '0.2s',
+                    motionDurationMid: '0.1s',
+                }
+            }}
+        >
             <AntdApp>
                 <AntdGlobal />
                 <NotificationProvider>
@@ -19,6 +32,14 @@ const App: React.FC = () => {
                     </Layout>
                 </NotificationProvider>
             </AntdApp>
+        </ConfigProvider>
+    );
+};
+
+const App: React.FC = () => {
+    return (
+        <SettingsProvider>
+            <AppContent />
         </SettingsProvider>
     );
 }
